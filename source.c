82727f2e711465621ac5ba1089cb659993cb24c3
benmimounrayen33-del <benmimounrayen33@gmail.com>
2025-12-20 21:57:12 +0100

    first commit, source code

---
+++ source.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+
+int lengthOfLongestSubstring(char *s) {
+    int freq[256] = {0};   // ASCII characters
+    int left = 0, maxLen = 0;
+
+    for (int right = 0; s[right] != '\0'; right++) {
+        freq[(unsigned char)s[right]]++;
+        while (freq[(unsigned char)s[right]] > 1) {
+            freq[(unsigned char)s[left]]--;
+            left++;
+        }
+
+        int windowLen = right - left + 1;
+        if (windowLen > maxLen)
+            maxLen = windowLen;
+    }
+
+    return maxLen;
+}
+
+bool isinfile(const char* filename, const char* text) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        return false;
+    }
+    char buffer[1024];
+    bool found = false;
+    while (fgets(buffer, sizeof(buffer), file)) {
+        if (strstr(buffer, text) != NULL) {
+            found = true;
+            break;
+        }
+    }
+    fclose(file);
+    return found;
+}
+
+void addtotextfile(const char* filename, const char* text) {
+    
+    int nb =lengthOfLongestSubstring((char*)text);
+    FILE* file = fopen(filename, "a");
+    if (file == NULL) {
+        return;
+    }
+    if (isinfile(filename, text)) {
+        return; // Text already exists in file
+    }
+    fprintf(file, "%s   %d\n", text, nb);
+    fclose(file);
+
+}
---
+++ source.h
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+
+#ifndef source_H
+#define source_H
+
+
+int lengthOfLongestSubstring(char* s);
+bool isinfile(const char* filename, const char* text);
+void addtotextfile(const char* filename, const char* text);
+ 
+
+
+
+#endif
