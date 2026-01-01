bab8eb9dcd4a28e44c50234f3531eacb0c0cbe5d
benmimounrayen33-del <benmimounrayen33@gmail.com>
2026-01-01 09:53:44 +0100

    fix logic

--- main.c
+++ main.c
@@ -10,12 +10,36 @@ int main() {
     char text[256];
     const char* filename = "testcases.txt";
 
-    printf("Enter a string: ");
+    printf("Enter a string: \n");
     fgets(text, sizeof(text), stdin);
     text[strcspn(text, "\n")] = 0;
+    if(strlen(text) == 0) {
+        printf("Empty string provided.\n");
+        return 1;
+    }
+    if(strlen(text) > 255) {
+        printf("String too long. Maximum length is 255 characters.\n");
+        return 1;
+    }
+    int nb = lengthOfLongestSubstring(text);
+    if((isinfile(filename, text))) {
+        printf("String already exists in file.\n");
 
-    addtotextfile(filename, text);
-    printf("Processed and added to file if not duplicate.\n");
+        printf("Length of longest substring without repeating characters: %d\n", nb);
+        return 1;
+    }else{
+        printf("String does not exist in file. Proceeding to add.\n");
+        addtotextfile(filename, text);
+        
+        printf("Length of longest substring without repeating characters: %d\n", nb);
+        printf("Processed and added to file if not duplicate.\n");
+    }
+    
+
+    
+    
+    
+
 
     return 0;
 }
